17966d57f9c95729d52aa8f8f8ada9d23f2e3720
Brarian <b.namonywa@gmail.com>
2019-05-10 18:07:55 -0500

    added scroll

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+
 function drop() {
   document.getElementById("drop_id").classList.toggle("show");
 }
 
 function goto(element){
+  console.log(element)
   document.querySelector(element).scrollIntoView({ 
     behavior: 'smooth' 
   });
